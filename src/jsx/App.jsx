45207734b331f38067ad10fa90274e167a4dac8b
Teemo Tebest <teemo.tebest@gmail.com>
2020-08-05 15:06:19 +0200

    Fixed flickering and added possibility to manually select year

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -23,7 +23,8 @@ class App extends Component {
     this.chartRef = React.createRef();
 
     this.state = {
-      'current_data':{
+      backgrounds:[],
+      current_data:{
         hurricanes:0,
         major_hurricanes:0
       },
@@ -41,7 +42,7 @@ class App extends Component {
   }
   getData() {
     d3.csv('./data/data - data.csv').then((data) => {
-      let images = new Array()
+      let backgrounds = [];
       let bar_chart_data = {
         labels:[],
         datasets:[{
@@ -59,8 +60,7 @@ class App extends Component {
         }]
       };
       data.map((values, i) => {
-        images[i] = new Image();
-        images[i].src = path_prefix + 'img/1600px-' + values.year + '_Atlantic_hurricane_season_summary_map-min.png';
+        backgrounds.push(path_prefix + 'img/1600px-' + values.year + '_Atlantic_hurricane_season_summary_map-min.png');
         if (values.year === '1990' || values.year === '2000' || values.year === '2010' || values.year === '2019') {
           bar_chart_data.labels.push(values.year);
         }
@@ -76,11 +76,13 @@ class App extends Component {
         bar_chart_data.datasets[2].data.push(parseInt(values.hurricanes) + parseInt(values.major_hurricanes));
         bar_chart_data.datasets[2].backgroundColor.push('rgba(254, 91, 89, 0.45)');
       });
-      this.createChart(bar_chart_data)
-      this.createInterval(data);
+      this.setState((state, props) => ({
+        backgrounds:backgrounds,
+        data:{...data}
+      }), this.createChart(bar_chart_data, data));
     });
   }
-  createChart(bar_chart_data) {
+  createChart(bar_chart_data, data) {
     let ctx = this.chartRef.current.getContext('2d');
 
     chart = new Chart(ctx, {
@@ -92,6 +94,7 @@ class App extends Component {
         legend:{
           display:false
         },
+        onClick:this.handleClick.bind(this),
         scales:{
           xAxes: [{
             display:true,
@@ -122,6 +125,16 @@ class App extends Component {
       },
       type:'bar'
     });
+    this.createInterval(data);
+  }
+  handleClick(evt) {
+    clearInterval(interval);
+    let active_element = chart.getElementAtEvent(evt)[0];
+    if (active_element) {
+      console.log(active_element._index)
+      console.log(this.state.data)
+      this.changeData(active_element._index, this.state.data[active_element._index])
+    }
   }
   createInterval(data) {
     let i = 0;
@@ -129,25 +142,33 @@ class App extends Component {
       this.appRef.current.style.display = 'block';
       if (data[0]) {
         let current_data = data.shift();
-        this.setState((state, props) => ({
-          current_data:current_data,
-          img_src:'1600px-' + current_data.year + '_Atlantic_hurricane_season_summary_map-min.png'
-        }));
-        if (i > 0) {
-          chart.data.datasets[0].backgroundColor[i - 1] = 'rgba(0, 247, 243, 0.45)';
-          chart.data.datasets[1].backgroundColor[i - 1] = 'rgba(254, 142, 0, 0.45)';
-          chart.data.datasets[2].backgroundColor[i - 1] = 'rgba(254, 91, 89, 0.45)';
-        }
-        chart.data.datasets[0].backgroundColor[i] = 'rgba(0, 247, 243, 1)';
-        chart.data.datasets[1].backgroundColor[i] = 'rgba(254, 142, 0, 1)';
-        chart.data.datasets[2].backgroundColor[i] = 'rgba(254, 91, 89, 1)';
-        chart.update(0);
+        this.changeData(i, current_data);
         i++;
       }
       else {
         clearInterval(interval);
       }
     }, 1250);
+  }
+  changeData(i, current_data) {
+    this.setState((state, props) => ({
+      current_data:current_data,
+      img_src:'1600px-' + current_data.year + '_Atlantic_hurricane_season_summary_map-min.png'
+    }));
+    chart.data.datasets[0].backgroundColor = chart.data.datasets[0].backgroundColor.map((background) => {
+      return 'rgba(0, 247, 243, 0.45)';
+    });
+    chart.data.datasets[1].backgroundColor = chart.data.datasets[1].backgroundColor.map((background) => {
+      return 'rgba(254, 142, 0, 0.45)';
+    });
+    chart.data.datasets[2].backgroundColor = chart.data.datasets[2].backgroundColor.map((background) => {
+      return 'rgba(254, 91, 89, 0.45)';
+    });
+    console.log(chart.data.datasets[2].backgroundColor)
+    chart.data.datasets[0].backgroundColor[i] = 'rgba(0, 247, 243, 1)';
+    chart.data.datasets[1].backgroundColor[i] = 'rgba(254, 142, 0, 1)';
+    chart.data.datasets[2].backgroundColor[i] = 'rgba(254, 91, 89, 1)';
+    chart.update(0);
   }
   // shouldComponentUpdate(nextProps, nextState) {}
   // static getDerivedStateFromProps(props, state) {}
@@ -168,6 +189,11 @@ class App extends Component {
         </div>
         <div className={style.chart_container}>
           <canvas id={style.chart} ref={this.chartRef}></canvas>
+        </div>
+        <div className={style.hidden}>
+          {this.state.backgrounds && this.state.backgrounds.map((background, i) => {
+            return (<img src={background} key={i}/>)
+          })}
         </div>
         <div className={style.background_container} style={{backgroundImage:'url(' + path_prefix + 'img/' + this.state.img_src + ')'}}></div>
       </div>
